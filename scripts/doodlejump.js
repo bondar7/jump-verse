f1dd8fa97011d12084ad29e35943e8ac5b4d256b
Maksym <maksimbondar225@gmail.com>
2024-11-24 13:02:03 +0100

    create game loop.

--- scripts/doodlejump.js
+++ scripts/doodlejump.js
@@ -28,4 +28,15 @@ window.onload = () => {
   doodlerLeftImg = new Image();
   doodlerLeftImg.src = "../images/doodler-left.png";
 
+  requestAnimationFrame(update);
+}
+
+// GAME LOOP
+// When you're playing the game, if you move to the left or to the right - you're changing x and y coordinates.
+// So you need to update the canvas by redrawing it. For that reason we need the game loop.
+function update() {
+  requestAnimationFrame(update);
+
+  //doodler
+  context.drawImage(doodler.getImg(), doodler.getX(), doodler.getY(), doodler.getWidth(), doodler.getHeight());
 }
