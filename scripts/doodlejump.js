e3fccb3e989c94490fbd6d097962d1a9a68934a1
Maksym <maksimbondar225@gmail.com>
2024-11-24 22:03:17 +0100

    change the image of doodler. make doodler movable (only by x).

--- scripts/doodlejump.js
+++ scripts/doodlejump.js
@@ -11,6 +11,10 @@ const doodler = new Doodler(board.getWidth(), board.getHeight());
 let doodlerRightImg;
 let doodlerLeftImg;
 
+//physics 
+let velocityX = 0;
+let velocityY = 0;
+
 window.onload = () => {
   boardHTML = document.getElementById("board");
   boardHTML.width = board.getWidth();
@@ -19,16 +23,19 @@ window.onload = () => {
 
   //load images
   doodlerRightImg = new Image();
-  doodlerRightImg.src = "../images/doodler-right.png";
+  doodlerRightImg.src = "../images/doodler-right-1.png";
   doodler.setImg(doodlerRightImg); 
   doodlerRightImg.onload = () => {
     context.drawImage(doodler.getImg(), doodler.getX(), doodler.getY(), doodler.getWidth(), doodler.getHeight());
   }
 
   doodlerLeftImg = new Image();
-  doodlerLeftImg.src = "../images/doodler-left.png";
+  doodlerLeftImg.src = "../images/doodler-left-1.png";
 
   requestAnimationFrame(update);
+
+  //keylistener to move doodler
+  document.addEventListener("keydown", moveDoodler);
 }
 
 // GAME LOOP
@@ -36,7 +43,24 @@ window.onload = () => {
 // So you need to update the canvas by redrawing it. For that reason we need the game loop.
 function update() {
   requestAnimationFrame(update);
+  context.clearRect(0, 0, board.getWidth(), board.getHeight());
 
   //doodler
+  doodler.setX(doodler.getX() + velocityX);
+  if (doodler.getX() > board.getWidth()) {
+    doodler.setX(0 - doodler.getWidth());
+  } else if (doodler.getX() < 0 - doodler.getWidth()) {
+    doodler.setX(board.getWidth());
+  }
   context.drawImage(doodler.getImg(), doodler.getX(), doodler.getY(), doodler.getWidth(), doodler.getHeight());
+}
+
+function moveDoodler(event) {
+  if (event.code == "ArrowRight" || event.code == "KeyD") { //move right
+    velocityX = 4;
+    doodler.setImg(doodlerRightImg);
+  } else if (event.code == "ArrowLeft" || event.code == "KeyE") { //move left
+    velocityX = -4;
+    doodler.setImg(doodlerLeftImg);
+  }
 }
